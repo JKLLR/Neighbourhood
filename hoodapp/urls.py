b8c6c4e8f6cae4673069d8cb8fe0dea105b0c131
JKLLR <jeffhuria@gmail.com>
2022-06-19 17:16:12 +0300

    add new amenity template

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns=[
     path('login/',views.signin,name='login'),
     path('register/',views.register,name='register'),
     path('updateprofile/',views.edit_profile,name='edit_profile'),
+    path('newhood/',views.new_hood,name='new_hood'),
     path('business/',views.new_business,name='new_business'),
+    path('amenity/',views.new_amenity,name='new_amenity'),
 ]
