bd37953ca1ff7db2327744f48e390c8a636655b1
JKLLR <jeffhuria@gmail.com>
2022-06-19 11:29:09 +0300

    update models

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -81,3 +81,23 @@ class Post(models.Model):
    def __str__(self):
       return self.content
    
+class Amenities(models.Model):
+  
+   name = models.CharField(max_length=50)
+   tel = models.IntegerField()
+   email = models.EmailField(max_length=254)
+   amenity_type = models.CharField(max_length=50)
+   hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.name
+
+
+class Comment(models.Model):
+   
+   comment = models.CharField(max_length=50)
+   comment_on = models.ForeignKey(Post, on_delete=models.CASCADE)
+   comment_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.comment
